d3a25e339623b4c57de019003aedd36d34c2bb8a
MediocreFantasy <113609788+MediocreFantasy@users.noreply.github.com>
2022-09-15 09:16:51 -0400

    Initial file upload of full project folder

---
+++ Pluralsight/Pluralsight/wwwroot/js/JavaScript.js
@@ -0,0 +1,20 @@
+﻿$(document).ready(function () {
+
+    var basicInfo = document.getElementById("basicInfo");
+
+    var saveBasicInfo = $("button#saveBasicInfo");
+    saveBasicInfo.on("click", function () {
+        alert("Information saved!");
+    });
+
+    var infoBox = $(".infoBox");
+
+    var loginToggle = $("#loginToggle");
+    var popupForm = $(".popupForm");
+
+    loginToggle.on("click", function () {
+        popupForm.toggle();
+    })
+
+
+});
